022afd3bd978194aa4615b3f49193cb8ae2e22cb
webdevcrazier <devcrazier@gmail.com>
2020-06-05 18:23:57 -0600

    change

--- js/content-script.js
+++ js/content-script.js
@@ -1,13 +1,44 @@
 var iframe = document.createElement('iframe'); 
-iframe.style.background = "green";
 iframe.style.height = "100vh";
 iframe.style.width = "500px";
 iframe.style.position = "fixed";
 iframe.style.top = "0px";
 iframe.style.right = "0px";
-iframe.style.zIndex = "9000000000000000000";
-iframe.frameBorder = "none"; 
+iframe.style.zIndex = "90";
+// iframe.frameBorder = "none"; 
 iframe.src = chrome.extension.getURL("popup.html")
 
-document.body.appendChild(iframe);
-document.body.style.width = 'calc(100vw - 510px)'
+var mainframe = document.createElement('iframe'); 
+mainframe.style.height = "100vh";
+mainframe.style.width = 'calc(100vw - 510px)';
+mainframe.style.position = "fixed";
+mainframe.style.top = "0px";
+mainframe.style.zIndex = "90";
+// mainframe.frameBorder = "none"; 
+mainframe.src = window.location.href;
+// mainframe.src = "https://wwf.org";
+
+document.body.innerHTML = ''
+document.body.className = ''
+document.body.appendChild(mainframe);
+document.body.appendChild(iframe)
+
+var script = document.createElement("script");
+
+// Add script content
+
+script.innerHTML = `$('#urlinput').keypress(function(e) {
+    e.pre
+    console.log('key', e.which)
+    if (e.which == 13) {
+        console.log('key', e.which)
+        url = $('#urlinput').val();
+        console.log(url)
+        window.location.href = url;
+    }
+
+})`;
+
+// Append
+
+document.body.appendChild(script);
---
+++ js/custome.js
@@ -0,0 +1,133 @@
+let home = `<div class = "head" id="header">
+<div class= "row">
+    <div class="col float-left">
+        <button id="hometag" onclick="myFunction()" >Home</button>
+    </div>
+    <div class="col float-right">
+        <img src="./src/avarta.png" class="circleimage"/>
+        <i class='fas fa-bars' style='font-size:18px'></i> 
+    </div>
+</div>
+<div class= "row headerbotcontainer">
+    <div class="inputurlcontainer">
+        <input id="urlinput"/>
+    </div>
+    <div class="sharebuttoncontainer">
+        <i class='far fa-bell'></i>
+        <button class="sharebutton">SHARE</button>
+    </div>
+</div>
+</div>
+<div class = "containter body">
+
+</div>
+<div class="footer">
+<div class="row commentInput">
+    <div class="left_input">
+        <input type="text" class="input" placeholder="Comment this page..."/>                        
+    </div>
+    <div class="icon_container">
+        <i class="fa fa-smile-o" style="font-size:36px"></i>
+    </div>                
+    <div class="icon_container">
+        <i class="fa fa-telegram" style="font-size:36px"></i>
+    </div>                
+</div>
+<div class="row button-container">
+    <button class="btn btn-primary button">Add spot comments</button>
+</div>
+
+</div>`
+
+let main = `
+<div class = "head" id="header">
+<div class= "row">
+    <div class="col float-left">
+        <img class = "greenlight" src="./src/Greenlight.png"/>
+        <b> &nbsp;Greenlight</b>
+    </div>
+    <div class="col float-right">
+        <i class='far fa-bell'></i>
+        <img src="./src/avarta.png" class="circleimage"/>
+        <i class='fas fa-bars' style='font-size:18px'></i> 
+    </div>
+</div>
+</div>
+<div class = "containter body">
+<div>
+    <img class="curv" src="./src/curv.png" />
+</div><br/>
+<div class="helpPannel">
+    <div class="row comment">
+        <div class="col float-left ">
+            Add a Comment
+        </div>
+        <div class="col">
+            <div class="row float-right">
+                <div class="key"></div> &nbsp;+&nbsp;
+                <div class="key"></div> &nbsp;+&nbsp;
+                <div class="key"></div>
+                <div>&nbsp;&nbsp;&nbsp;&nbsp;</div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col float-left">
+            Toggle Inspector Mode
+        </div>
+        <div class="col">
+            <div class="row float-right">
+                <div class="key"></div> &nbsp;+&nbsp; 
+                <div class="key"></div> &nbsp;+&nbsp;
+                <div class="key"></div>
+                <div>&nbsp;&nbsp;&nbsp;&nbsp;</div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<div class="footer">
+<div class="row commentInput">
+    <div class="left_input">
+        <input type="text" class="input" placeholder="Comment this page..."/>                        
+    </div>
+    <div class="icon_container">
+        <i class="fa fa-smile-o" style="font-size:36px"></i>
+    </div>                
+    <div class="icon_container">
+        <i class="fa fa-telegram" style="font-size:36px"></i>
+    </div>                
+</div>
+<div class="row button-container">
+    <button class="btn btn-primary button" id="addcomment">Add spot comments</button>
+</div>
+
+</div>
+`
+let homecontainer = $('#homecontainer');
+let maincontainer = $('#maincontainer');
+homecontainer.hide()
+$('#addcomment').click(function(){
+    homecontainer.hide();
+    maincontainer.show();
+    // console.log("click button")
+    // container.empty();
+    // container.append(home)
+});
+
+$('#hometag').click(function() {
+    homecontainer.show();
+    maincontainer.hide();
+});
+$('#urlinput').keypress(function(e) {
+    e.pre
+    console.log('key', e.which)
+    if (e.which == 13) {
+        console.log('key', e.which)
+        url = $('#urlinput').val();
+        url = 'https://www.google.com/'
+        console.log(url)
+        window.location.href = url;
+    }
+
+})
